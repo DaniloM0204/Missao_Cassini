bc4bce8ac490fb334ab1f5fdbac3491fad43a922
Danilo M <danilo.machado@grad.ufsc.br>
2025-10-25 15:32:45 -0300

    Adicionado vetores planetarios

--- Calculos_Cassini.py
+++ Calculos_Cassini.py
@@ -64,10 +64,15 @@ class Orbita:
         vypq = np.sqrt((μ / self.p)) * (self.e + np.cos(f))
         return vxpq, vypq
 
-
+# Vetores da Cassini e Planetas
 Vetores_Cassini = {}
+Vetores_Planetas = {}
+
+# Condições iniciais para leitura
 encontroucabecalho = False
 secao = None
+
+
 try:
     with open("Cassini_Vetor.csv", "r") as f:
 
@@ -111,11 +116,57 @@ except FileNotFoundError:
     print("O arquivo não foi encontrado.")
 
 
+try:
+    with open("Vetores_Planetas.csv", "r") as f:
+
+        for line in f:
+            line_branco = line.strip()
+
+            if not line_branco or line_branco.startswith(";") or line_branco.startswith(" "):
+                continue
+            # Separa os dados
+            if line.startswith("Vetores_"):
+                split_line = line.strip().split(";")
+                secao = split_line[0].strip()
+                encontroucabecalho = False
+
+            elif line.startswith("X"):
+                encontroucabecalho = True
+
+            # Organiza vetores posição e velocidade
+
+            elif encontroucabecalho and secao:
+                vetores = []
+
+                try:
+                    for i in range (6):
+                        vetor = float(line.split(";")[i].strip().replace(",","."))
+                        vetores.append(vetor)
+                    rvec = np.array(vetores[0:3])
+                    vvec = np.array(vetores[3:6])
+
+                    nova_orbita = Orbita()
+                    nova_orbita.secao_nome = secao
+                    nova_orbita.vet_estado(μ_Sol, rvec, vvec)
+                    Vetores_Planetas[secao] = nova_orbita
+
+                    encontroucabecalho = False
+                except Exception as e:
+                    print(f"Erro ao processar a linha: {line}. Erro: {e}")
+                    encontroucabecalho = False
+except FileNotFoundError:
+    print("O arquivo não foi encontrado.")
+
 # Teste
 try:
     print(f"  Semi-eixo maior (a): {Vetores_Cassini['Vetor_Terra_Venus'].a:.4f} AU")
 except KeyError:
-    print(" A chave 'Vetor_Terra_Venus' não foi encontrada no dicionário.")
+    print(" A chave não foi encontrada no dicionário.")
+try:
+    print(f"  Semi-eixo maior (a): {Vetores_Planetas['Vetores_Terra'].a:.4f} AU")
+except KeyError:
+    print(" A chave não foi encontrada no dicionário.")
+
 
 
 with open("Dados_Orbita_Cassini.txt", "w") as f:
@@ -128,5 +179,14 @@ with open("Dados_Orbita_Cassini.txt", "w") as f:
         f.write(f"  Velocidade (v): {orbita_obj.vvec} AU/dia\n")
         f.write("\n")
 
+with open("Dados_Orbita_Planetas.txt", "w") as f:
+    for secao_nome, orbita_obj in Vetores_Planetas.items():
+        f.write(f"Dados para {secao_nome}:\n")
+        f.write(f"  Semi-eixo maior (a): {orbita_obj.a:.4f} AU\n")
+        f.write(f"  Excentricidade (e): {orbita_obj.e:.4f}\n")
+        f.write(f"  Semilado reto (p): {orbita_obj.p:.4f} AU\n")
+        f.write(f"  Posicao (r): {orbita_obj.rvec} AU\n")
+        f.write(f"  Velocidade (v): {orbita_obj.vvec} AU/dia\n")
+        f.write("\n")
 
 
